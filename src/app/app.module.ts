757e14b42634ebb49a290e2d36d799e272bbd75a
SidhanthAmara <68857000+SidhanthAmara@users.noreply.github.com>
2020-07-28 00:32:44 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { WorkTogetherComponent } from './work-together/work-together.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { RecentPostComponent } from './recent-post/recent-post.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    WorkTogetherComponent,
+    ProjectsComponent,
+    RecentPostComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/work-together/work-together.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-work-together',
+  templateUrl: './work-together.component.html',
+  styleUrls: ['./work-together.component.css']
+})
+export class WorkTogetherComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
